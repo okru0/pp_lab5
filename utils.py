ef5a7d29be33de59be1d43941e37e72691e1aac8
272511 <272511@student.pwr.edu.pl>
2025-05-07 16:17:48 +0200

    Added docstrings

--- utils.py
+++ utils.py
@@ -1,13 +1,29 @@
-# Exemplary calculator functions
+"""
+utils.py - Simple calculator
 
-def add (a: int , b: int) -> int:
+Includes simple functions:
+-add
+-subtract
+-multiply
+-divide
+"""
+
+
+def add(a: int, b: int) -> int:
+    """Returns the sum of two integers a and b"""
     return a + b
 
-def subtract (a: int, b: int) -> int:
+
+def subtract(a: int, b: int) -> int:
+    """Returns the result of subtracting integer b from integer a"""
     return a - b
 
-def multiply (a: int, b: int) -> int:
+
+def multiply(a: int, b: int) -> int:
+    """Returns the product of two integers a and b"""
     return a * b
 
-def divide (a: int, b: int) -> float:
+
+def divide(a: int, b: int) -> float:
+    """Returns the result of dividing two integers a and b"""
     return a / b
